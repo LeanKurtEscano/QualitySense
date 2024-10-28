8dc2b08197b92687ff76ce3c87e4b7c0ded0f560
Lean Kurt <leankurtescano@gmail.com>
2024-10-28 16:25:40 +0800

    Adjusted Token life cycle

--- Backend/datasense_app/datasense_app/settings.py
+++ Backend/datasense_app/datasense_app/settings.py
@@ -115,8 +115,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=2), 
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=3),
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=1), 
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=14),
    
 }
 
