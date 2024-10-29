0d891618ba19ce3392ba3289a7fef2bd70690cfe
Lean Kurt <leankurtescano@gmail.com>
2024-10-29 14:00:47 +0800

    Made some changes to the notification pop up in the generate page

--- Frontend/qualitysense-app/src/Components/Warning.tsx
+++ Frontend/qualitysense-app/src/Components/Warning.tsx
@@ -15,9 +15,9 @@ const Warning: React.FC<Toggle> = ({setToggleWarning}) => {
             <div className="flex items-center justify-between">
                 <div className="flex items-center">
                     <div className="flex flex-col ml-3">
-                        <div className="font-medium leading-none text-gray-100 mb-1">Too many requests</div>
+                        <div className="font-medium leading-none text-gray-100 mb-1">Upload Limit</div>
                         <p className="text-sm text-gray-500 leading-none mt-1">
-                          Please wait again for a few minutes.
+                           Please wait 3 minutes before trying again.
                         </p>
                     </div>
                 <div className='pb-5 cursor-pointer' onClick={removeNotif}>
