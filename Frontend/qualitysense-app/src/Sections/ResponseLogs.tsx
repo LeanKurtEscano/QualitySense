8b9efd6cf67c4b85efcfe3ed20a6485da0caae07
Lean Kurt <leankurtescano@gmail.com>
2024-11-03 15:51:20 +0800

    Fixed the response log layout errors

--- Frontend/qualitysense-app/src/Sections/ResponseLogs.tsx
+++ Frontend/qualitysense-app/src/Sections/ResponseLogs.tsx
@@ -7,6 +7,7 @@ import { faTrash } from '@fortawesome/free-solid-svg-icons';
 import { cleanText } from '../Utils';
 import Download from '../Components/Download';
 import Paginator2 from '../Components/Paginator2';
+import { TbUserQuestion } from 'react-icons/tb';
 
 
 interface PaginateItems {
@@ -39,6 +40,7 @@ const ResponseLogs: React.FC = () => {
 
         if (response) {
             const filterData = data.filter((item: { id: number; }) => item.id !== id)
+            getGenerated();
             setData(filterData);
         }
     }
@@ -58,12 +60,12 @@ const ResponseLogs: React.FC = () => {
 
     useEffect(() => {
 
-        if(data.length === 0) {
+        if (data.length === 0) {
             setEmptyHistory("You haven't generated any history yet.");
 
         }
 
-    },[data])
+    }, [data])
 
     const getGenerated = async () => {
         const userToken = localStorage.getItem('access_token');
@@ -99,22 +101,26 @@ const ResponseLogs: React.FC = () => {
 
     const generatedData = data.filter((item: { id: any; }) => item.id === result);
 
+    useEffect(() => {
+       
+    },[generatedData])
+
     return (
         <section className="w-full min-h-screen sm:overflow-y-auto h-auto flex flex-col overflow-hidden bg-darkbg">
 
             {emptyHistory ? (
                 <div className='flex  items-center justify-center pt-24 pl-16'>
-                <h1 className='text-slate-200 md:text-4xl text-[12px] pr-14 font-bold'>{emptyHistory}</h1>
+                    <h1 className='text-slate-200 md:text-4xl text-[12px] pr-14 font-bold'>{emptyHistory}</h1>
                 </div>
             ) : (
                 <div>
-                    <div className='w-full flex justify-center pt-10 md:pr-80'>
+                    <div className='w-full flex justify-center pt-10 mb-4 md:pr-80'>
                         <Paginator2 pageNumber={pageNumber} totalPages={totalPages} setPageNumber={setPageNumber} />
 
                     </div>
-                    <div className="flex pb-10 md:flex-row items-center justify-center  flex-col h-full">
-                       
-                        <div className="flex-1 pt-20 flex flex-col justify-start md:pl-40 min-h-screen items-center bg-darkbg">
+                    <div className="flex  md:flex-row items-center justify-center  flex-col h-auto min-h-screen">
+
+                        <div className="flex  flex-col md:pl-40 min-h-screen items-center bg-darkbg">
                             {data.map((item: any, index: any) => (
                                 <div
                                     onClick={() => selectGenerated(item.id)}
@@ -140,7 +146,7 @@ const ResponseLogs: React.FC = () => {
                                             ...
                                         </div>
                                         {showOptions && item.id === ItemID ? (
-                                            <div className='flex border  border-gray-800 bg-darkbg rounded-lg shadow-lg absolute flex-col items-center justify-center  right-7 mt-40 md:right-[420px] md:mb-20 md:ml-16 p-4'>
+                                            <div className='flex border  border-gray-800 bg-darkbg rounded-lg shadow-lg absolute flex-col items-center justify-center  right-7 mt-40 md:right-[480px] md:mb-20 md:ml-16 p-4'>
                                                 <div className='flex pr-8 flex-row items-center justify-center cursor-pointer hover:bg-gray-800 p-2 rounded-md' onClick={() => deleteData(item.id)}>
                                                     <FontAwesomeIcon icon={faTrash} className='text-red-600' />
                                                     <p className='ml-2 text-red-600'>Delete</p>
@@ -154,20 +160,29 @@ const ResponseLogs: React.FC = () => {
                             ))}
                         </div>
 
-                        <div className="flex-1 pt-16 h-full overflow-hidden flex md:pl-0 pl-11  justify-center md:pb-20 items-center bg-darkbg">
-                            <div className="max-h-screen w-full p-4 overflow-y-auto">
-                                {generatedData.map((item: any, index: any) => {
-                                    const cleanedText = cleanText(item.result || '');
-                                    const lines = cleanedText.split('\n');
+                        <div className="flex-1 min-h-screen  pt-1 overflow-hidden flex md:pl-0 pl-11 justify-center md:pb-20 items-center bg-darkbg">
+                            <div className="max-h-screen min-h-screen pt-9 flex-grow  w-full p-4 overflow-y-auto">
+                                {generatedData.length > 0 ? (
+                                    generatedData.map((item: any, index: any) => {
+                                        const cleanedText = cleanText(item.result || '');
+                                        const lines = cleanedText.split('\n');
 
-                                    return (
-                                        <div key={index} className='mb-4'>
-                                            <p className='md:whitespace-pre-wrap text-darktext3 md:w-auto w-[250px]'>{lines.join('\n')}</p>
-                                        </div>
-                                    );
-                                })}
+                                        return (
+                                            <div key={index} className="mb-4">
+                                                <p className="md:whitespace-pre-wrap text-darktext3 md:w-auto w-[250px]">
+                                                    {lines.join('\n')}
+                                                </p>
+                                            </div>
+                                        );
+                                    })
+                                ) : (
+                                    <div className="flex items-center justify-center h-full">
+                                        <p className="text-gray-500">No data available</p>
+                                    </div>
+                                )}
                             </div>
                         </div>
+
                     </div>
                 </div>
             )}
