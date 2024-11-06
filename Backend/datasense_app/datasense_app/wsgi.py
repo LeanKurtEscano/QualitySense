6625e3f01806f4a1a6702f66c0052fc8a92260f1
Lean Kurt <leankurtescano@gmail.com>
2024-11-06 09:43:20 +0800

    Change settings

--- Backend/datasense_app/datasense_app/wsgi.py
+++ Backend/datasense_app/datasense_app/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'datasense_app.datasense_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'datasense_app.settings')
 
 application = get_wsgi_application()
