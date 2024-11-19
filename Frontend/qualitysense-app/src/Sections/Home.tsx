64c96528b1cd93d108a1c90a4d18224695a498e8
Lean Kurt <leankurtescano@gmail.com>
2024-11-19 22:12:37 +0800

    Initialize files

--- Frontend/qualitysense-app/src/Sections/Home.tsx
+++ Frontend/qualitysense-app/src/Sections/Home.tsx
@@ -21,18 +21,18 @@ const Home:React.FC = () => {
       <div className='absolute z-0 w-[30%] h-[30%] bottom-40 right-20  transform -rotate-12 blue__gradient'></div>
 
       <div className='flex flex-row z-10 relative'>
-        <h1 className='text-slate-200 md:text-6xl text-3xl font-bold mr-2'>Welcome to</h1>
+        <h1 className='text-slate-200 md:text-6xl text-3xl font-bold mr-2'>Welcome to </h1>
         <h1 className='bg-gradient-to-br md:text-6xl text-3xl from-cyan-500 to-blue-500 bg-clip-text text-transparent font-bold'>QualitySense</h1>
       </div>
 
       <div className='md:w-[700px] w-[380px] items-center mt-4 mb-4 z-10 relative'>
         <p className='text-slate-200 font-normal md:text-lg text-sm text-center'>
           Effortlessly assess your data quality and uncover valuable insights with AI, helping you achieve smarter data-driven decisions.
+          
         </p>
       </div>
 
       <div className='bg-spotlight z-10 relative'></div>
-
       <div className='z-10 relative'>
         <button onClick={goToGenerate}
           type="button"
